63bd549d1a7320144f2ebad2cfa72995bde1753b
wangmeng <wm921781999@163.com>
2020-06-20 22:36:47 +0800

    初始化项目结构

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_admin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_admin.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
