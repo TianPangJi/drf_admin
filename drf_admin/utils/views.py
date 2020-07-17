4da418167beb6b6a26049dbb4de2d82bcf3584ff
wangmeng <921781999@qq.com>
2020-07-17 20:24:31 +0800

    修改批量删除mixin

--- drf_admin/utils/views.py
+++ drf_admin/utils/views.py
@@ -12,20 +12,20 @@ from rest_framework.response import Response
 
 class MultipleDestroyMixin:
     """
-    自定义批量删除mixin, 与GenericViewSet配合使用
+    自定义批量删除mixin, 与GenericViewSet配合使用, 路由仅适合使用as_view()
     """
 
     @action(methods=['delete'], detail=False)
     def multiple_delete(self, request, *args, **kwargs):
         delete_ids = request.data.get('ids')
         if not delete_ids:
-            return Response(data={'error': '参数错误,ids为必传参数'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={'detail': '参数错误,ids为必传参数'}, status=status.HTTP_400_BAD_REQUEST)
         if not isinstance(delete_ids, list):
-            return Response(data={'error': 'ids格式错误,必须为List'}, status=status.HTTP_400_BAD_REQUEST)
-        instance = self.get_object()
-        querysets = instance.filter(id__in=delete_ids)
-        if len(delete_ids) != len(querysets):
-            return Response(data={'error': '删除数据不存在'}, status=status.HTTP_400_BAD_REQUEST)
-        for queryset in querysets:
+            return Response(data={'detail': 'ids格式错误,必须为List'}, status=status.HTTP_400_BAD_REQUEST)
+        queryset = self.get_queryset()
+        del_queryset = queryset.filter(id__in=delete_ids)
+        if len(delete_ids) != len(del_queryset):
+            return Response(data={'detail': '删除数据不存在'}, status=status.HTTP_400_BAD_REQUEST)
+        for queryset in del_queryset:
             queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
