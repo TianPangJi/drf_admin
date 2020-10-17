ee6f6fc38267dbfa8769d2119046c4e6885178e5
wangmeng <921781999@qq.com>
2020-10-17 18:42:44 +0800

    添加cmdb资产管理模块, 并创建models模型

---
+++ drf_admin/apps/cmdb/serializers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+""" 
+@author   : Wang Meng
+@github   : https://github.com/tianpangji 
+@software : PyCharm 
+@file     : __init__.py.py
+@create   : 2020/10/17 17:10
+"""
+
