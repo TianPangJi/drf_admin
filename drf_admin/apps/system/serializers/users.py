b16bfaae72f8b2120f320412033340eba4c1a7a2
wangmeng <921781999@qq.com>
2020-12-09 20:25:39 +0800

    修改用户序列化器添加默认密码功能

--- drf_admin/apps/system/serializers/users.py
+++ drf_admin/apps/system/serializers/users.py
@@ -47,51 +47,11 @@ class UsersSerializer(serializers.ModelSerializer):
         return [{'id': role.id, 'desc': role.desc} for role in obj.roles.all()]
 
     def create(self, validated_data):
+        user = super().create(validated_data)
         # 添加默认密码
-        validated_data['password'] = settings.DEFAULT_PWD
-
-        # 未直接调用父类create方法(原因: 新建用户时使用 create_user() 方法)
-        raise_errors_on_nested_writes('create', self, validated_data)
-        ModelClass = self.Meta.model
-
-        # Remove many-to-many relationships from validated_data.
-        # They are not valid arguments to the default `.create()` method,
-        # as they require that the instance has already been saved.
-        info = model_meta.get_field_info(ModelClass)
-        many_to_many = {}
-        for field_name, relation_info in info.relations.items():
-            if relation_info.to_many and (field_name in validated_data):
-                many_to_many[field_name] = validated_data.pop(field_name)
-
-        try:
-            instance = ModelClass._default_manager.create_user(**validated_data)
-        except TypeError:
-            tb = traceback.format_exc()
-            msg = (
-                    'Got a `TypeError` when calling `%s.%s.create()`. '
-                    'This may be because you have a writable field on the '
-                    'serializer class that is not a valid argument to '
-                    '`%s.%s.create()`. You may need to make the field '
-                    'read-only, or override the %s.create() method to handle '
-                    'this correctly.\nOriginal exception was:\n %s' %
-                    (
-                        ModelClass.__name__,
-                        ModelClass._default_manager.name,
-                        ModelClass.__name__,
-                        ModelClass._default_manager.name,
-                        self.__class__.__name__,
-                        tb
-                    )
-            )
-            raise TypeError(msg)
-
-        # Save many-to-many relationships after the instance is created.
-        if many_to_many:
-            for field_name, value in many_to_many.items():
-                field = getattr(instance, field_name)
-                field.set(value)
-
-        return instance
+        user.set_password(settings.DEFAULT_PWD)
+        user.save()
+        return user
 
 
 class UsersPartialSerializer(serializers.ModelSerializer):
