a6eaae13f8f9f66d539f85ae7efe1bda0b112b0b
wangmeng <921781999@qq.com>
2020-12-26 18:41:03 +0800

    添加部门修改时的验证

--- drf_admin/apps/system/serializers/departments.py
+++ drf_admin/apps/system/serializers/departments.py
@@ -8,6 +8,7 @@
 """
 from rest_framework import serializers
 
+from drf_admin.common.models import get_child_ids
 from drf_admin.utils.views import TreeSerializer
 from system.models import Departments
 
@@ -20,6 +21,13 @@ class DepartmentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Departments
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        # 验证服部门父部门不能为其本身或其子部门
+        departments_id = get_child_ids(instance.id, Departments)
+        if validated_data.get('pid') and validated_data.get('pid').id in departments_id:
+            raise serializers.ValidationError('父部门不能为其本身或其子部门')
+        return super().update(instance, validated_data)
 
 
 class DepartmentsTreeSerializer(TreeSerializer):
