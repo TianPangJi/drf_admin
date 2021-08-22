3610c76baf959ae6d2b23bd215824797f6a0b306
wangmeng <921781999@qq.com>
2021-08-22 19:48:39 +0800

    抽离validate

--- drf_admin/utils/views.py
+++ drf_admin/utils/views.py
@@ -7,6 +7,7 @@
 """
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -24,17 +25,22 @@ class MultipleDestroyMixin:
     class MultipleDeleteSerializer(serializers.Serializer):
         ids = serializers.ListField(required=True, write_only=True)
 
-    @swagger_auto_schema(request_body=MultipleDeleteSerializer)
-    def multiple_delete(self, request, *args, **kwargs):
-        delete_ids = request.data.get('ids')
+    def validate_ids(self, delete_ids):
+        # 验证object传入的删除id列表
         if not delete_ids:
-            return Response(data={'detail': '参数错误,ids为必传参数'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('参数错误,ids为必传参数')
         if not isinstance(delete_ids, list):
-            return Response(data={'detail': 'ids格式错误,必须为List'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('ids格式错误,必须为List')
         queryset = self.get_queryset()
         del_queryset = queryset.filter(id__in=delete_ids)
         if len(delete_ids) != del_queryset.count():
-            return Response(data={'detail': '删除数据不存在'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('删除数据不存在')
+        return del_queryset
+
+    @swagger_auto_schema(request_body=MultipleDeleteSerializer)
+    def multiple_delete(self, request, *args, **kwargs):
+        delete_ids = request.data.get('ids')
+        del_queryset = self.validate_ids(delete_ids)
         del_queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
