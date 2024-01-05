eb896fdeb16ab244cb60bdc5eab013ce287921a9
林子白 <abcde12345326@gmail.com>
2024-01-05 22:45:29 +0800

    修改完成

--- drf_admin/apps/oauth/views/home.py
+++ drf_admin/apps/oauth/views/home.py
@@ -21,15 +21,16 @@ Users = get_user_model()
 class HomeAPIView(APIView):
     """
     get:
-    系统主页--数据显示
+    系统首頁--数据显示
 
-    获取系统主页数据, status: 200(成功), return: 系统主页数据
+    获取系统首頁数据, status: 200(成功), return: 系统首頁数据
     """
 
     def get(self, request):
         data = dict()
         conn = get_redis_connection('user_info')
-        data['visits'] = int(conn.get('visits').decode()) if conn.get('visits') else 0
+        data['visits'] = int(conn.get('visits').decode()
+                             ) if conn.get('visits') else 0
         data['users'] = Users.objects.all().count()
         data['online_users'] = OnlineUsers.objects.all().count()
         data['assets'] = Assets.objects.all().count()
