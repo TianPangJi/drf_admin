bcfc69919e892b42dfef4c756806cee84f83657e
林子白 <abcde12345326@gmail.com>
2024-01-28 14:05:55 +0800

    db Temp

--- drf_admin/apps/courses/models.py
+++ drf_admin/apps/courses/models.py
@@ -7,16 +7,21 @@ class Book(models.Model):
     書籍
     """
     DIFFICULTY_CHOICES = [
-        ('easy', _('簡單')),
-        ('medium', _('中等')),
-        ('hard', _('困難')),
+        ('繪本', _('繪本')),
+        ('橋梁書', _('橋梁書')),
+        ('初階文字書', _('初階文字書')),
+        ('中階文字書', _('中階文字書')),
+        ('高階文字書', _('高階文字書')),
     ]
 
     CATEGORY_CHOICES = [
-        ('fiction', _('小說')),
-        ('nonfiction', _('非小說')),
-        ('science', _('科學')),
-        ('history', _('歷史')),
+        ('小說', _('小說')),
+        ('非小說', _('非小說')),
+        ('科學', _('科學')),
+        ('歷史', _('歷史')),
+        ('故事', _('故事')),
+        ('人社', _('人社')),
+        ('其他', _('其他')),
         # 可根據需求添加更多選項
     ]
 
@@ -60,3 +65,28 @@ class BookRating(models.Model):
 
     def __str__(self):
         return f'{self.book.name} - {self.rating}'
+
+
+
+
+
+    #   <el-form-item label="分類">
+    #     <el-select v-model="bookForm.category" placeholder="請選擇">
+    #       <el-option label="小說" value="小說" />
+    #       <el-option label="非小說" value="非小說" />
+    #       <el-option label="科學類" value="科學類" />
+    #       <el-option label="歷史類" value="歷史類" />
+    #       <el-option label="故事類" value="故事類" />
+    #       <el-option label="人社類" value="人社類" />
+    #       <el-option label="其他" value="其他" />
+    #     </el-select>
+    #   </el-form-item>
+    #   <el-form-item label="難度">
+    #     <el-select v-model="bookForm.difficulty" placeholder="請選擇">
+    #       <el-option label="繪本" value="繪本" />
+    #       <el-option label="橋梁書" value="橋梁書" />
+    #       <el-option label="初階文字書" value="初階文字書" />
+    #       <el-option label="中階文字書" value="中階文字書" />
+    #       <el-option label="高階文字書" value="高階文字書" />
+    #     </el-select>
+    #   </el-form-item>
